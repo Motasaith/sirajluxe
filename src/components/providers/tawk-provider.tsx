e8da04b5c0185414218c3b3a41ee6dd37a8a5d4f
Motasaith <saithmota@gmail.com>
2026-03-16 00:09:32 +0500

    fix: update TawkProvider script loading strategy for improved performance

--- src/components/providers/tawk-provider.tsx
+++ src/components/providers/tawk-provider.tsx
@@ -4,7 +4,7 @@ import Script from "next/script";
 
 export function TawkProvider() {
   return (
-    <Script id="tawk-to" strategy="afterInteractive">
+    <Script id="tawk-to" strategy="lazyOnload">
       {`
         var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
         (function(){
@@ -13,7 +13,11 @@ export function TawkProvider() {
           s1.src='https://embed.tawk.to/69a45e51a705f91c36464794/1jil10rpu';
           s1.charset='UTF-8';
           s1.setAttribute('crossorigin','*');
-          s0.parentNode.insertBefore(s1,s0);
+          if (s0 && s0.parentNode) {
+            s0.parentNode.insertBefore(s1,s0);
+          } else {
+            document.head.appendChild(s1);
+          }
         })();
       `}
     </Script>
